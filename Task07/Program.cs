b8cb4fba33cc4d1406a26a5e7fb2529cc320fc36
Илья Смаль <johngoblikon328@gmail.com>
2023-09-17 19:42:58 +1000

    Added Task03, Task05, Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 7. Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю цифру
+этого числа.
+456 -> 6
+782 -> 2
+918 -> 8 */
+
+Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 0)
+{
+    number *= -1;
+}
+
+if (99 < number && number < 1000)
+{
+    int lastNumbmer = number % 10;
+    Console.WriteLine($"{number} -> {lastNumbmer}");
+}
+else if (-99 > number && number > -1000)
+{
+    Console.WriteLine("Неверное значение!");
+}
+
