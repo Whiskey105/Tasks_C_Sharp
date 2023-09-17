9ea4a66405b6ae6088a48e33a739490f22d7ad2b
Илья Смаль <johngoblikon328@gmail.com>
2023-09-17 18:46:33 +1000

    Added Task01 resolved

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿int firstNumber, secondNubmer;
+
+Console.WriteLine("Введите первое число: ");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+secondNubmer = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNubmer*secondNubmer)
+{
+    Console.WriteLine($"первое число = {firstNumber}, второе число = {secondNubmer} -> Да");
+}
+else
+{
+    Console.WriteLine($"первое число = {firstNumber}, второе число = {secondNubmer} -> Нет");
+}
+
