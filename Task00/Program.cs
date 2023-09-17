01eac01fb350eed277d6efdc18d2576bc904498a
Илья Смаль <johngoblikon328@gmail.com>
2023-09-17 18:25:37 +1000

    Task00 Complited

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, 
+1. которая на вход принимает число и 
+2. выдаёт его квадрат (число умноженное на само себя).
+3. вывод результата
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49 */
+
+Console.Write("Введите целое число для возведения в степень: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+// Console.WriteLine(number + " -> " + square);
+Console.WriteLine($"{number} -> {square}");
