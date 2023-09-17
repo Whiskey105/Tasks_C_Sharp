85eeb021735a531760a867f360703185cec362d2
Илья Смаль <johngoblikon328@gmail.com>
2023-09-17 19:08:36 +1000

    Refactoring

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,11 +1,19 @@
-﻿int firstNumber, secondNubmer;
+﻿/* Напишите программу, которая на вход принимает два
+числа и проверяет, является ли первое число квадратом
+второго.
+a = 25, b = 5 -> да
+a = 2, b = 10 -> нет
+a = 9, b = -3 -> да
+a = -3 b = 9 -> нет */
+
+int firstNumber, secondNubmer;
 
 Console.WriteLine("Введите первое число: ");
 firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 secondNubmer = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber == secondNubmer*secondNubmer)
+if (firstNumber == secondNubmer * secondNubmer)
 {
     Console.WriteLine($"первое число = {firstNumber}, второе число = {secondNubmer} -> Да");
 }
