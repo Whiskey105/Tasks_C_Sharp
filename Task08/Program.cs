8d036046949e0a9d0bf826a7fea0ad39312e827c
Илья Смаль <johngoblikon328@gmail.com>
2023-09-22 17:30:00 +1000

    Task08 Resolution

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = $"{number} ->";
+
+for (int i = 2; i <= number; i+=2)
+{
+    result += $" {i},";
+}
+
+result = result.Remove(result.Length - 1);
+Console.WriteLine(result);
