230b873a0f044c0d3c62fea220008aea3d278452
Илья Смаль <johngoblikon328@gmail.com>
2023-09-23 18:18:59 +1000

    Task11 Solution

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого
+числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int RemoveMidDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    return firstDigit * 10 + lastDigit;
+}
+
+int randNumber = new Random().Next(100, 1000); 
+int twoDigitNumber = RemoveMidDigit(randNumber);
+
+Console.WriteLine($"{randNumber} -> {twoDigitNumber}");
+
