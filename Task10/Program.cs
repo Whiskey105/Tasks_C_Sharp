2c51d9ef2a520bc4aa6cc5cf43b1e20151eebb26
Илья Смаль <johngoblikon328@gmail.com>
2023-09-24 16:24:36 +1000

    Task10 Solution

---
+++ Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetSecondDigit(int num)
+{
+    return num / 10 % 10;
+}
+
+int Promt (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Promt("Введите трехзнаечное число: ");
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Out of range exception");
+    return;
+}
+
+Console.WriteLine($"{number} -> {GetSecondDigit(number)}");
