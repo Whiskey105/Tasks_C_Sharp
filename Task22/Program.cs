3c43039e05a3632d5f3275c5c2c03f7cf64f57bb
Илья Смаль <johngoblikon328@gmail.com>
2023-09-24 18:34:41 +1000

    Task22 Solution

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 22: Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N. */
+
+void PrintTableSquare (int num)
+{
+    Console.WriteLine($"Таблица кваратов чиcла {num}");
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i,3} -> {i*i,5}");
+    }
+}
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintTableSquare(number);
+
