5b5cace07ddf216a332b2fe20bedc237eb2d9cb0
Илья Смаль <johngoblikon328@gmail.com>
2023-09-25 08:41:16 +1000

    Task23 Solution

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintTableSquare (int num)
+{
+    Console.WriteLine($"Таблица кубов чиcла {num}");
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i,3} -> {Math.Pow(i, 3),5}");
+    }
+}
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintTableSquare(number);
