708dd2f744915230e7bf4b69f74f61e4c538954d
Илья Смаль <johngoblikon328@gmail.com>
2023-10-01 16:30:48 +1000

    Task27 Solution

---
+++ Task27/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int Promt(string message)
+{
+    Console.Write(message);
+    
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+
+    return result;
+}
+
+int SumOfDigits(int num)
+{
+    int result = 0;
+
+    if (num < 0)
+    {
+        num *= -1;
+    }
+
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+
+    return result;
+}
+
+
+int number = Promt("Введите число: ");
+int sumOfDigits = SumOfDigits(number);
+
+Console.WriteLine($"{number} -> {sumOfDigits}");
