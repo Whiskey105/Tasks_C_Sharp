d243be177831a30bdc4f577d3540146f27a477d4
Илья Смаль <johngoblikon328@gmail.com>
2023-10-09 16:38:50 +1000

    Task50 CreateIncreasingMatrix and PrintArray added

---
+++ Task50/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 50: Напишите программу, которая на вход
+принимает позиции элемента в двумерном массиве, и
+возвращает значение этого элемента или же указание,
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+int[,] CreateIncreasingMatrix(int n, int m, int k)
+{
+    int[,] matrix = new int[n, m];
+    Random rnd = new Random();
+    int incr = 1;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = incr;
+            incr += k;
+        }
+    }
+
+    return matrix;
+}
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arr2d = CreateIncreasingMatrix(4,4,5);
+
+PrintArray(arr2d);
