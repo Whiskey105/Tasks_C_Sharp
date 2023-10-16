3b0d0b18ea991411bae5c0acbfe86a4f210c3d24
Илья Смаль <johngoblikon328@gmail.com>
2023-10-16 18:15:31 +1000

    Task53 Solution

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -10,6 +10,7 @@
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
     int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
